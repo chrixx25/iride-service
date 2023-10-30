6b06d8018ec5ed748e8e0e059460977124082034
chrixx25 <delosreyeschris77@gmail.com>
2023-10-30 13:57:48 +0800

    update cors origin

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    origin: "http://localhost:3000",
   }),
 );
 
