ad68ccc06ee109a90fa4c46577dbaccac896d1af
chrixx25 <delosreyeschris77@gmail.com>
2023-11-13 00:21:40 +0800

    change folder name for arc and api

--- api/index.js
+++ api/index.js
@@ -1,28 +1,3 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
+const app = require("../src/index");
 
-const app = express();
-const PORT = process.env.APP_PORT || 5000;
-const UserRouter = require("./routers/user");
-const ScheduleRouter = require("./routers/schedule");
-const BusRouter = require("./routers/bus");
-const HistoryRouter = require("./routers/history");
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(
-  cors({
-    origin: process.env.FE_URL,
-  }),
-);
-
-app.use("/iride/users", UserRouter);
-app.use("/iride/schedules", ScheduleRouter);
-app.use("/iride/buses", BusRouter);
-app.use("/iride/histories", HistoryRouter);
-
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server is running....`);
-});
+module.exports = app;
